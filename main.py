23f9b918d302615d4a2b18796c9c746d7f3b96df
Tobit Lill <tobit.lill@gmail.com>
2026-08-04 09:35:00 +0200

    add copper farming

--- main.py
+++ main.py
@@ -6,12 +6,21 @@ load_dotenv()
 
 character_name = os.getenv("CHARACTER_NAME")
 
-move(0, 1)
 
-while True:
-    health, max_health = get_character_health()
-    if health < max_health * 0.5:
-        rest()
-    else:
-        fight()
+def endless_leveling():
+    move(0, 1)
+    while True:
+        health, max_health = get_character_health()
+        if health < max_health * 0.5:
+            rest()
+        else:
+            fight()
 
+
+def endless_copper_farming():
+    move(2, 0)
+    while True:
+        gather()
+
+
+endless_copper_farming()
